6a421cd75199bf2c85c6f787882a0a6f75a7a3be
iddqd2d@gmail.com <32167Ant>
2019-03-01 20:45:33 +0200

    b2c1

--- src/carautopilot/CurrentCar.java
+++ src/carautopilot/CurrentCar.java
@@ -6,7 +6,7 @@ public class CurrentCar {
     public static void main(String[] args) {
         Car car = new Car(LocalDate.now(), Engine.DIESEL, 180, 20, 4, 3, 100);
         System.out.println(car);
-        System.out.println();
+     //   System.out.println();
         car.getWheel(0).wipeTire(50);
         car.removeAllPassenger();
         car.addPassenger();
